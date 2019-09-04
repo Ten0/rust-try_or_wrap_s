99480a7a08bea79033254b350e1b378584e2f4df
Thomas BESSOU <thomas.bessou@hotmail.fr>
2019-09-04 18:49:38 +0200

    Rename to try_or_wrap_s
    - Wrap to anything
    - Support options

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 //! This crate provides a macro similar to the old `try!` macro, or to the `?`, except it wraps
-//! the error in an additional `Ok`.
+//! the error in something else.
 //!
 //! This is useful if you want to use a `?` for easily returning say, invalid input errors,
 //! but you can't do so because you have an additional `Result` level for handling internal errors of a different nature.
 //!
 //! This macro allows you to do so:
 //!
-//! ```
+//! ```ignore
 //! fn foo(input: Input) -> Result<Result<FinalOutput, InvalidInputError>, DatabaseError> {
-//!     let validated_input: ValidatedInput = try_wrap_ok!(validate_input_with_database(input)?);
+//!     let validated_input: ValidatedInput = try_or_wrap!(Ok, validate_input_with_database(input)?);
 //!     Ok(Ok(do_stuff_with_validated_input(validated_input)?))
 //! }
 //!
@@ -16,24 +16,35 @@
 //! ```
 
 #[macro_export]
-/// Helper macro to wrap `?` into an `Ok`
+/// Helper macro to wrap `?` into something else, for `Result`
 ///
 /// # Example
-/// ```
+/// ```ignore
 /// fn foo(input: Input) -> Result<Result<FinalOutput, InvalidInputError>, DatabaseError> {
-///     let validated_input: ValidatedInput = try_wrap_ok!(validate_input_with_database(input)?);
+///     let validated_input: ValidatedInput = try_or_wrap!(Ok, validate_input_with_database(input)?);
 ///     Ok(Ok(do_stuff_with_validated_input(validated_input)?))
 /// }
 ///
 /// fn validate_input_with_database(input: Input) -> Result<Result<ValidatedInput, InvalidInputError>, DatabaseError>;
 /// ````
-macro_rules! try_wrap_ok {
-    ($expr:expr) => {
+macro_rules! try_or_wrap {
+    ($wrapper:expr, $expr:expr) => {
         match $expr {
             std::result::Result::Ok(val) => val,
             std::result::Result::Err(err) => {
-                return Ok(std::result::Result::Err(std::convert::From::from(err)))
+                return $wrapper(std::result::Result::Err(std::convert::Into::into(err)))
             }
+        }
+    };
+}
+
+/// Same as `try_or_wrap`, but for `Option`
+#[macro_export]
+macro_rules! try_or_wrap_opt {
+    ($wrapper:expr, $expr:expr) => {
+        match $expr {
+            std::option::Option::Some(val) => val,
+            std::option::Option::None => return $wrapper(std::option::Option::None),
         }
     };
 }
